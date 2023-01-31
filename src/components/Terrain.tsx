f5e44ef0b015d9d60a8b433252b87e30fc8c494c
Philippe Négrel-Jerzy <negreljerzy.philippe@gmail.com>
2023-01-31 23:06:53 +0100

    feat: implement shader loading

--- src/components/Terrain.tsx
+++ src/components/Terrain.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Suspense, useEffect, useState } from "react";
 import limboFrag from "../shaders/limbo.frag";
 import limboVert from "../shaders/limbo.vert";
 
@@ -9,22 +9,40 @@ export default function Terrain({
   center?: [number, number, number];
   size?: [number, number, number];
 }) {
+  const [shaders, setShaders] = useState<{
+    vert: string;
+    frag: string;
+  }>();
+
+  useEffect(() => {
+    Promise.all([fetch(limboVert), fetch(limboFrag)])
+      .then((responses) => Promise.all(responses.map((r) => r.text())))
+      .then((values) => {
+        setShaders({
+          vert: values[0],
+          frag: values[1],
+        });
+      });
+  }, []);
+
   return (
-    <mesh
-      position={[0, 0, 0]}
-      rotation={[-Math.PI / 2, 0, 0]}
-      scale={[1 / 1024, 1 / 1024, 1 / 1024]}
-    >
-      <planeBufferGeometry args={[1024, 1024, 256, 256]} />
-      <shaderMaterial
-        uniforms={{
-          // Feed the scaling constant for the heightmap
-          bumpScale: { value: 50 },
-        }}
-        // Feed the shaders as strings
-        vertexShader={limboVert}
-        fragmentShader={limboFrag}
-      />
-    </mesh>
+    <Suspense fallback={null}>
+      <mesh
+        position={[0, 0, 0]}
+        rotation={[-Math.PI / 2, 0, 0]}
+        scale={[1 / 1024, 1 / 1024, 1 / 1024]}
+      >
+        <planeBufferGeometry args={[1024, 1024, 256, 256]} />
+        <shaderMaterial
+          uniforms={{
+            // Feed the scaling constant for the heightmap
+            bumpScale: { value: 50 },
+          }}
+          // Feed the shaders as strings
+          vertexShader={shaders?.vert}
+          fragmentShader={shaders?.frag}
+        />
+      </mesh>
+    </Suspense>
   );
 }
